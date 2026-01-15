123ca97813822434cfea0db497f7e4361fd2b1d0
SeyeTine <tine.seye@gmail.com>
2026-01-15 17:40:05 -0500

    Commit 2 : Constructeur pour la classe etu

---
+++ Exercice 4.py
@@ -0,0 +1,8 @@
+# Écrire un programme de votre choix qui illustre le principe de classe.
+# Pendant les cours de POO on nous explique souvent avec la classe voiture ou la classe étudiante
+
+class Etudiant:
+    def __init__(self, nom, note):
+        self.nom = nom
+        self.note = note
+
