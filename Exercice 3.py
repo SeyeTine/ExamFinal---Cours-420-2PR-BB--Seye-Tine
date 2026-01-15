0d059f04a9da48453b624ea8bcd632d3222ebde8
SeyeTine <tine.seye@gmail.com>
2026-01-15 15:22:12 -0500

    Commit 2 : Initialisation

---
+++ Exercice 3.py
@@ -0,0 +1,8 @@
+# Creation de la classe outils
+class Outils:
+    def __init__(self, n):
+        #On initialise ce qu'on veut calculer apres
+        self.min = n
+        self.max = n
+        self.somme = n
+        self.moyenne = n
