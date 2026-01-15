e573c0789fd2846b81c7546e8c5339c35d5dadbd
SeyeTine <tine.seye@gmail.com>
2026-01-15 17:42:50 -0500

    Commit 3 : Methode afficher

--- Exercice 4.py
+++ Exercice 4.py
@@ -2,7 +2,12 @@
 # Pendant les cours de POO on nous explique souvent avec la classe voiture ou la classe étudiante
 
 class Etudiant:
-    def __init__(self, nom, note):
+    def __init__(self, prenom, nom, note):
         self.nom = nom
+        self.prenom = prenom
         self.note = note
 
+    def affiche(self):
+        print("Prénom : ", self.prenom)
+        print("Nom : ", self.nom)
+        print("Note", self.note)
